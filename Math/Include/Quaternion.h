9f32927ab7e721d21bbef031ff8645e3db8322ff
Nick Cullen <nmcullen91@gmail.com>
2016-03-18 13:27:01 +0000

    Added quaternion addition multiplication and subtraction

--- Math/Include/Quaternion.h
+++ Math/Include/Quaternion.h
@@ -17,4 +17,21 @@ public:
 	// Destructors
 	inline ~Quaternion() {};
 
+	// Addition
+	static Quaternion Add(const Quaternion& q1, const Quaternion& q2);
+	Quaternion operator+(const Quaternion& other);
+
+	// Multiplication
+	static Quaternion Multiply(const Quaternion& q1, const Quaternion& q2);
+	static Quaternion Multiply(const Quaternion& q1, const float s);
+	Quaternion operator*(const Quaternion& other);
+
+	// Subtraction
+	static Quaternion Subtract(const Quaternion& q1, const Quaternion& q2);
+	Quaternion operator-(const Quaternion& other);
+
 };
+
+// Scalar multiplication
+Quaternion operator*(const Quaternion& q1, const float scalar) { return Quaternion::Multiply(q1, scalar); }
+Quaternion operator*(const float scalar, const Quaternion& q1) { return Quaternion::Multiply(q1, scalar); }
--- Math/Source/Quaternion.cpp
+++ Math/Source/Quaternion.cpp
@@ -11,3 +11,38 @@
 //{
 //
 //}
+
+Quaternion Quaternion::Add(const Quaternion& q1, const Quaternion& other)
+{
+	return Quaternion(q1.w + other.w, q1.x + other.x, q1.y + other.y, q1.z + other.z);
+}
+
+Quaternion Quaternion::operator+(const Quaternion& other)
+{
+	return Quaternion::Add(*this, other);
+}
+
+Quaternion Quaternion::Multiply(const Quaternion& q1, const Quaternion& other)
+{
+	return Quaternion(q1.w * other.w, q1.x * other.x, q1.y * other.y, q1.z * other.z);
+}
+
+Quaternion Quaternion::Multiply(const Quaternion& q1, const float s)
+{
+	return Quaternion(q1.w * s, q1.x * s, q1.y * s, q1.z * s);
+}
+
+Quaternion Quaternion::operator*(const Quaternion& other)
+{
+	return Quaternion::Multiply(*this, other);
+}
+
+Quaternion Quaternion::Subtract(const Quaternion& q1, const Quaternion& other)
+{
+	return Quaternion(q1.w - other.w, q1.x - other.x, q1.y - other.y, q1.z - other.z);
+}
+
+Quaternion Quaternion::operator-(const Quaternion& other)
+{
+	return Quaternion::Subtract(*this, other);
+}
